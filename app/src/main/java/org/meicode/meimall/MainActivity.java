f8945914e2c92c2d4a5c6cda42cd40007ef8974e
Meisam <Meisam@meiCode.org>
2020-06-02 14:06:42 +0400

    The source code for the video called 'Second Sample Application - First Cart Fragment' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/meimall/MainActivity.java
@@ -0,0 +1,71 @@
+package org.meicode.meimall;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
+import static org.meicode.meimall.AllCategoriesDialog.ALL_CATEGORIES;
+import static org.meicode.meimall.AllCategoriesDialog.CALLING_ACTIVITY;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
+    private MaterialToolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initViews();
+        Utils.initSharedPreferences(this);
+
+        setSupportActionBar(toolbar);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.drawer_open, R.string.drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.cart:
+                        Toast.makeText(MainActivity.this, "Cart Selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.categories:
+                        AllCategoriesDialog dialog = new AllCategoriesDialog();
+                        Bundle bundle = new Bundle();
+                        bundle.putStringArrayList(ALL_CATEGORIES, Utils.getCategories(MainActivity.this));
+                        bundle.putString(CALLING_ACTIVITY, "main_activity");
+                        dialog.setArguments(bundle);
+                        dialog.show(getSupportFragmentManager(), "all categories dialog");
+                        break;
+                    default:
+                        break;
+                }
+                return false;
+            }
+        });
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, new MainFragment());
+        transaction.commit();
+    }
+
+    private void initViews() {
+        drawer = findViewById(R.id.drawer);
+        navigationView = findViewById(R.id.navigationView);
+        toolbar = findViewById(R.id.toolbar);
+    }
+}
